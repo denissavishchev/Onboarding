e17da1e5a679820f5c706ae539e50bb5890ca87c
Denis Savishchev <denis.savishchev@gmail.com>
2024-11-04 05:26:35 +0100

    Initial Commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Devis on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
